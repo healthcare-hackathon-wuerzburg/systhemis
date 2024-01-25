4ba8d5781e95492fbbcb8aca0e38653a1f709ab0
Dominik Rose <Dominik.Rose.extern@itsg.de>
2024-01-25 16:05:09 +0100

    Added Stepper to Registration

--- frontend/HealConnect/src/app/components/content/register/register.component.ts
+++ frontend/HealConnect/src/app/components/content/register/register.component.ts
@@ -1,50 +1,56 @@
 import { Component } from '@angular/core';
 import { AbstractControl, FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { confirmFieldValidator } from '../../shared/validator/CustomFormValidators';
-import { MatError, MatFormField, MatLabel } from '@angular/material/form-field';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInput } from '@angular/material/input';
 import { MatOption, MatSelect } from '@angular/material/select';
 import { NgIf } from '@angular/common';
+import { MatStepperModule } from '@angular/material/stepper';
 
 @Component({
   selector: 'app-register',
   standalone: true,
   imports: [
     ReactiveFormsModule,
-    MatFormField,
+    MatFormFieldModule,
     MatInput,
-    MatLabel,
     MatSelect,
     MatOption,
-    MatError,
-    NgIf
+    NgIf,
+    MatStepperModule
   ],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss'
 })
 export class RegisterComponent {
-  registrationForm: FormGroup;
+  firstRegistrationStep: FormGroup;
+  secondRegistrationStep: FormGroup;
 
   public constructor(private fb: FormBuilder) {
-    this.registrationForm = fb.group({
+    this.firstRegistrationStep = fb.group({
         email: ['',  [Validators.required, Validators.email]],
         password: ['', Validators.required],
         confirmPassword: ['', Validators.required],
         userType: ['', Validators.required]
       },
-      {validators: confirmFieldValidator('password', 'confirmPassword')})
+      {validators: confirmFieldValidator('password', 'confirmPassword')}
+    );
+    this.secondRegistrationStep = fb.group( {
+      test: ['', Validators.required],
+      test3: ['', Validators.required]
+    })
   }
 
   get emailControl(): AbstractControl {
-    return this.registrationForm.controls['email'];
+    return this.firstRegistrationStep.controls['email'];
   }
   get passwordControl(): AbstractControl {
-    return this.registrationForm.controls['password'];
+    return this.firstRegistrationStep.controls['password'];
   }
   get confirmPasswordControl(): AbstractControl {
-    return this.registrationForm.controls['confirmPassword'];
+    return this.firstRegistrationStep.controls['confirmPassword'];
   }
   get formGroupError(): any {
-    return this.registrationForm.errors;
+    return this.firstRegistrationStep.errors;
   }
 }
