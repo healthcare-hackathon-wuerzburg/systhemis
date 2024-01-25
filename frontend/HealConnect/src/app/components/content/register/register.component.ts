58b535927f394dad537dd5733cdd78499bc86d6f
Dominik Rose <Dominik.Rose.extern@itsg.de>
2024-01-25 13:19:49 +0100

    added basic registration form

--- frontend/HealConnect/src/app/components/content/register/register.component.ts
+++ frontend/HealConnect/src/app/components/content/register/register.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [
+    ReactiveFormsModule
+  ],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss'
 })
 export class RegisterComponent {
+  registrationForm: FormGroup;
+
+  public constructor(private fb: FormBuilder) {
+    this.registrationForm = fb.group({
+      email: ['', Validators.required],
+      password: ['', Validators.required],
+      confirmPassword: ['', Validators.required, ],
+      userType: ['', Validators.required]
+    })
+  }
 
 }
