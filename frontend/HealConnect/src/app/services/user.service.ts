20050c5017b93f0b5a4750bfd9333455383a355c
Dominik Rose <Dominik.Rose.extern@itsg.de>
2024-01-25 19:27:29 +0100

    faked login

--- frontend/HealConnect/src/app/services/user.service.ts
+++ frontend/HealConnect/src/app/services/user.service.ts
@@ -1,17 +1,29 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, of, throwError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
+  private userNameAndPasswordList = new Map<string, string>([
+    ['userName', 'hallo123'],
+    ['example_user', 'secret'],
+    ['dr_jones', 'geheim'],
+    ['support123', 'jaja'],
+    ['MacGyver', 'sythemisag']
+  ]);
 
   constructor(private http: HttpClient) {
   }
 
-  login(value: any): Observable<any> {
-    return this.http.post('/api/login', value);
+  login(value: { username: string, password: string }): Observable<any> {
+    if(this.userNameAndPasswordList.has(value.username)) {
+      if(this.userNameAndPasswordList.get(value.username) == value.password) {
+        return of();
+      }
+    }
+    return throwError({status: 401})
   }
 
   register(value: any): Observable<any> {
