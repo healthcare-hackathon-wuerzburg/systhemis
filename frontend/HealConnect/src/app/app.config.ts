1859e61cafa1cb0a91653595c36290d73ddd6dab
Martin Awerjanow <mawerjanow@gmail.com>
2024-01-25 14:44:08 +0100

    update journal component with first form-fields

--- frontend/HealConnect/src/app/app.config.ts
+++ frontend/HealConnect/src/app/app.config.ts
@@ -4,15 +4,17 @@ import {provideRouter} from '@angular/router';
 import {routes} from './app.routes';
 import {provideServiceWorker} from '@angular/service-worker';
 import {BrowserAnimationsModule, provideAnimations} from "@angular/platform-browser/animations";
+import {provideNativeDateAdapter} from "@angular/material/core";
 
 export const appConfig: ApplicationConfig = {
     providers: [
-    provideRouter(routes),
-    provideServiceWorker('ngsw-worker.js', {
-        enabled: !isDevMode(),
-        registrationStrategy: 'registerWhenStable:30000'
-    }),
-    importProvidersFrom(BrowserAnimationsModule),
-    provideAnimations()
-],
+        provideRouter(routes),
+        provideServiceWorker('ngsw-worker.js', {
+            enabled: !isDevMode(),
+            registrationStrategy: 'registerWhenStable:30000'
+        }),
+        importProvidersFrom(BrowserAnimationsModule),
+        provideNativeDateAdapter(),
+        provideAnimations(),
+    ],
 };
