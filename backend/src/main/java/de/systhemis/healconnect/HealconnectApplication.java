d582bf8033a3e611bf7eab89b1f4625faa33eeea
Alexander Gerbig <agerbig@systhemis.de>
2024-01-25 11:17:38 +0100

    Initial Backend

---
+++ backend/src/main/java/de/systhemis/healconnect/HealconnectApplication.java
@@ -0,0 +1,13 @@
+package de.systhemis.healconnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealconnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealconnectApplication.class, args);
+	}
+
+}
