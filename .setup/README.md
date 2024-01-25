eafdd8e0226727bcd3ad4538b0f3fbadb4eb2eaf
Dominik Rose <Dominik.Rose.extern@itsg.de>
2024-01-25 11:09:39 +0100

    Added Environmental Variable Bat

---
+++ .setup/README.md
@@ -0,0 +1,14 @@
+# MDconfig Batch Scripts
+
+Scripts to make onboarding/developing easier.
+
+# Scripts
+
+## a-EnvVariable
+
+Sets the neceseary development Environment Variables.
+
+Expects to have Node Version Manager installed.
+
+Expects to have Adoptium 21.0.2 installed in default location. If not Installed please either change the path on your
+local machine.
---
+++ .setup/a-EnvVariable.bat
@@ -0,0 +1,29 @@
+REM --Check for Administrator rights and ask for permission if needed--
+@echo off
+:: BatchGotAdmin
+:-------------------------------------
+REM --> Check for permissions
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+
+REM --> If error flag set, we do not have admin.
+if '%errorlevel%' NEQ '0' (
+    ECHO Requesting administrative privileges...
+    goto UACPrompt
+) else ( goto gotAdmin )
+
+:UACPrompt
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    set params = %*:"=""
+    echo UAC.ShellExecute "cmd.exe", "/c %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs"
+
+    "%temp%\getadmin.vbs"
+    del "%temp%\getadmin.vbs"
+    exit /B
+
+:gotAdmin
+    pushd "%CD%"
+    CD /D "%~dp0"
+:--------------------------------------
+REM --Actual Script goes here--
+call EnvVariable.bat
+REM --End of Script--
