b8add039e158a00caa206f7c0e2a55b9546bdd99
Alexander Gerbig <agerbig@systhemis.de>
2024-01-26 00:02:30 +0100

    Zurück-Link eingefügt

--- frontend/HealConnect/src/app/components/content/info/info.component.spec.ts
+++ frontend/HealConnect/src/app/components/content/info/info.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { InfoComponent } from './info.component';
+import { provideRouter } from '@angular/router';
 
 describe('InfoComponent', () => {
   let component: InfoComponent;
@@ -8,10 +9,11 @@ describe('InfoComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [InfoComponent]
+      imports: [InfoComponent],
+      providers: [provideRouter([])]
     })
     .compileComponents();
-    
+
     fixture = TestBed.createComponent(InfoComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
