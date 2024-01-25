794b5d3c7281270f8dd25137a395fd775a77cdf1
Dominik Rose <Dominik.Rose.extern@itsg.de>
2024-01-25 14:12:13 +0100

    Error messages and correct Styling added

--- frontend/HealConnect/src/app/components/content/login/login.component.ts
+++ frontend/HealConnect/src/app/components/content/login/login.component.ts
@@ -1,23 +1,34 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AbstractControl, FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatDatepicker, MatDatepickerInput, MatDatepickerToggle } from '@angular/material/datepicker';
+import { MatError, MatFormField, MatLabel } from '@angular/material/form-field';
+import { MatInput } from '@angular/material/input';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [ReactiveFormsModule],
+    imports: [ReactiveFormsModule, MatDatepicker, MatDatepickerInput, MatDatepickerToggle, MatFormField, MatInput, MatLabel, MatError, NgIf],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+  loginFailed = false;
   loginForm: FormGroup;
 
   public constructor(private fb: FormBuilder) {
     this.loginForm = fb.group({
-      email: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
       password: ['', Validators.required]
     })
   }
 
+  get emailControl(): AbstractControl {
+    return this.loginForm.controls['email'];
+  }
+  get passwordControl(): AbstractControl {
+    return this.loginForm.controls['password'];
+  }
 
-
+  
 }
--- frontend/HealConnect/src/app/components/content/register/register.component.ts
+++ frontend/HealConnect/src/app/components/content/register/register.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AbstractControl, FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { confirmFieldValidator } from '../../shared/validator/CustomFormValidators';
+import { MatError, MatFormField, MatLabel } from '@angular/material/form-field';
+import { MatInput } from '@angular/material/input';
+import { MatOption, MatSelect } from '@angular/material/select';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-register',
   standalone: true,
   imports: [
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    MatFormField,
+    MatInput,
+    MatLabel,
+    MatSelect,
+    MatOption,
+    MatError,
+    NgIf
   ],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss'
@@ -16,7 +27,7 @@ export class RegisterComponent {
 
   public constructor(private fb: FormBuilder) {
     this.registrationForm = fb.group({
-        email: ['', Validators.required],
+        email: ['',  [Validators.required, Validators.email]],
         password: ['', Validators.required],
         confirmPassword: ['', Validators.required],
         userType: ['', Validators.required]
@@ -24,4 +35,17 @@ export class RegisterComponent {
       {validators: confirmFieldValidator('password', 'confirmPassword')})
   }
 
+  get emailControl(): AbstractControl {
+    return this.registrationForm.controls['email'];
+  }
+  get passwordControl(): AbstractControl {
+    return this.registrationForm.controls['password'];
+  }
+  get confirmPasswordControl(): AbstractControl {
+    return this.registrationForm.controls['confirmPassword'];
+  }
+  get formGroupError(): any {
+    console.log(this.registrationForm.errors)
+    return this.registrationForm.errors;
+  }
 }
